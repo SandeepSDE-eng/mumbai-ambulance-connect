ee059726976d4b1b91cd8f8a37d0e587ac5729f3
sandy-yadav <sandeep.sde.pss@gmail.com>
2025-08-14 01:52:51 +0530

    Your commit message

---
+++ gateway-service/src/test/java/com/mumbai/ambulance/gateway_service/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mumbai.ambulance.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-service/src/test/java/com/mumbai/ambulance/user_service/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mumbai.ambulance.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
